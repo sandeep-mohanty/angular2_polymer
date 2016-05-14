0ffaccf597ca38d8787c571d8413f15aa806f386
sandeep-mohanty <access.sandeep@gmail.com>
2016-05-14 22:23:15 +0530

    Checkbox example added
    Added paper-checkbox example

--- src/application/demo.ts
+++ src/application/demo.ts
@@ -13,12 +13,18 @@ import {CustomComponentAngular2} from "./components/custom_component";
     >{{buttonText}}</paper-button></div><br/><div align="center"><span *ngIf = "buttonMouseOver"><strong>Clicking will restore defaults</strong></span><br />
     <div align="center"><h2>Paper Icon Buttons</h2></div>
     <div align="center"> 
-        <paper-icon-button icon="favorite" [style.color] = "color.toLowerCase()" ></paper-icon-button>
-        <paper-icon-button icon="feedback" [style.color] = "color.toLowerCase()" ></paper-icon-button>
-        <paper-icon-button icon="folder-shared" [style.color] = "color.toLowerCase()" ></paper-icon-button>
-        <paper-icon-button icon="flag" [style.color] = "color.toLowerCase()"  ></paper-icon-button>
-        <paper-icon-button icon="help" [style.color] = "color.toLowerCase()" ></paper-icon-button>
-        <paper-icon-button icon="https" [style.color] = "color.toLowerCase()" ></paper-icon-button>
+        <paper-icon-button icon="favorite" [style.color] = "favorite.checked ? color.toLowerCase() : 'green'" ></paper-icon-button>
+        <paper-checkbox #favorite>Favorite</paper-checkbox>
+        <paper-icon-button icon="feedback" [style.color] = "feedback.checked ? color.toLowerCase() : 'green'" ></paper-icon-button>
+        <paper-checkbox #feedback>Feedback</paper-checkbox>
+        <paper-icon-button icon="folder-shared" [style.color] = "shared.checked ? color.toLowerCase() : 'green'" ></paper-icon-button>
+        <paper-checkbox #shared>Shared Folder</paper-checkbox>
+        <paper-icon-button icon="flag" [style.color] = "flag.checked ? color.toLowerCase() : 'green'"  ></paper-icon-button>
+        <paper-checkbox #flag>Flag</paper-checkbox>
+        <paper-icon-button icon="help" [style.color] = "help.checked ? color.toLowerCase() : 'green'" ></paper-icon-button>
+        <paper-checkbox #help>Help</paper-checkbox>
+        <paper-icon-button icon="https" [style.color] = "https.checked ? color.toLowerCase() : 'green'" ></paper-icon-button>
+        <paper-checkbox #https>Secured Connection</paper-checkbox>
     </div><br/>
     <div align="center"><strong>Set color:   </strong><input type="text" [(ngModel)] = "color"/></div><br/>
     <div align="center"><strong>Set Button Text:   </strong><input type="text" [(ngModel)] = "buttonText"/></div> <br />
